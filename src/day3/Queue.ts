68efd58299424f720af5ca51af0c9b550cdc5785
Brandon Courtney <brandon.h.courtney@gmail.com>
2024-07-22 16:17:08 -0600

    Queue   initiate class   remove .ts errors

--- src/day3/Queue.ts
+++ src/day3/Queue.ts
@@ -1,18 +1,37 @@
+class Item<T> {
+    public value: T;
+    public next: Item<T> | null;
+
+    constructor(value: T, next: Item<T> | null = null) {
+        this.value = value;
+        this.next = next;
+    }
+}
+
 export default class Queue<T> {
     public length: number;
-
-    
+    public head: Item<T> | null;
+    public tail: Item<T> | null;
 
     constructor() {
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
     }
 
-    enqueue(item: T): void {
+    decrementLength() {
+        this.length && --this.length;
+    }
 
-}
+    incrementLength() {
+        ++this.length;
+    }
+
+    enqueue(item: T): void {}
     deque(): T | undefined {
-
-}
+        return undefined;
+    }
     peek(): T | undefined {
-
-}
+        return undefined;
+    }
 }
