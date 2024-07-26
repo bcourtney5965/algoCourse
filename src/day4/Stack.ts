2faebc7f4009963e20b2d7515c1e17eeb77ac038
Brandon Courtney <brandon.h.courtney@gmail.com>
2024-07-25 23:20:03 -0600

    Initial Stack class setup

--- src/day4/Stack.ts
+++ src/day4/Stack.ts
@@ -1,18 +1,23 @@
+type Node<T> = {
+    value: number;
+    next: Node<T> | null;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = this.tail = undefined;
     }
 
-    push(item: T): void {
-
-}
+    push(item: T): void {}
     pop(): T | undefined {
-
-}
+        return;
+    }
     peek(): T | undefined {
-
-}
+        return;
+    }
 }
