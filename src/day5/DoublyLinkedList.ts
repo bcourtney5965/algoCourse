677be03d4dce81f25934c154d8210b6a8a437353
Brandon Courtney <brandon.h.courtney@gmail.com>
2024-08-01 15:34:44 -0600

    DoublyLinkedList fix type errors

--- src/day5/DoublyLinkedList.ts
+++ src/day5/DoublyLinkedList.ts
@@ -1,27 +1,18 @@
 export default class DoublyLinkedList<T> {
     public length: number;
 
-    
+    constructor() {}
 
-    constructor() {
-    }
-
-    prepend(item: T): void {
-
-}
-    insertAt(item: T, idx: number): void {
-
-}
-    append(item: T): void {
-
-}
+    prepend(item: T): void {}
+    insertAt(item: T, idx: number): void {}
+    append(item: T): void {}
     remove(item: T): T | undefined {
-
-}
+        return;
+    }
     get(idx: number): T | undefined {
-
-}
+        return;
+    }
     removeAt(idx: number): T | undefined {
-
-}
+        return;
+    }
 }
