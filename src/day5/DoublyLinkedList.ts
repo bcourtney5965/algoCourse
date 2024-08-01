45ebc79fc8e503792621712102525b50e83ee856
Brandon Courtney <brandon.h.courtney@gmail.com>
2024-08-01 16:43:55 -0600

    DoublyLinkedList   append() method

--- src/day5/DoublyLinkedList.ts
+++ src/day5/DoublyLinkedList.ts
@@ -25,7 +25,19 @@ export default class DoublyLinkedList<T> {
 
     prepend(item: T): void {}
     insertAt(item: T, idx: number): void {}
-    append(item: T): void {}
+    append(item: T): void {
+        const node = { value: item } as Node<T>;
+        this.increment();
+        if (this.head && this.tail) {
+            const tail = this.tail;
+            tail.next = node;
+            node.prev = tail;
+            this.tail = node;
+        } else {
+            this.head = node;
+            this.tail = node;
+        }
+    }
     remove(item: T): T | undefined {
         return;
     }
