1bf10411907f85b74c3ac364f525c9e2bc49d0ee
Brandon Courtney <brandon.h.courtney@gmail.com>
2024-08-01 15:43:18 -0600

    DoublyLinkedList   setup class   setup Node helper class

--- src/day5/DoublyLinkedList.ts
+++ src/day5/DoublyLinkedList.ts
@@ -1,7 +1,27 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+    next?: Node<T>;
+};
+
 export default class DoublyLinkedList<T> {
     public length: number;
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
-    constructor() {}
+    constructor() {
+        this.length = 0;
+        this.head = undefined;
+        this.tail = undefined;
+    }
+
+    increment(): void {
+        ++this.length;
+    }
+
+    decrement(): void {
+        this.length && --this.length;
+    }
 
     prepend(item: T): void {}
     insertAt(item: T, idx: number): void {}
