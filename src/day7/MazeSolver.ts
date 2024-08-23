31db2b3d253a5cbab26596fd911c54ecf328bc6e
Brandon Courtney <brandon.h.courtney@gmail.com>
2024-08-23 16:40:06 -0600

    MazeSolver   - base case (rough outline)

--- src/day7/MazeSolver.ts
+++ src/day7/MazeSolver.ts
@@ -1,3 +1,48 @@
+type Point = {
+    x: number;
+    y: number;
+};
+
+const locationIsOutsideMaze = (location: Point, maze: string[]): boolean => {
+    const { x, y } = location;
+    const width: number = maze[0]?.length || 0;
+    const height: number = maze.length || 0;
+    return x < 0 || x > width - 1 || y < 0 || y > height - 1;
+};
+
+const alreadyVisited = (maze: string[], location: Point): boolean => {
+    return maze[location.y][location.x] === "1";
+};
+
+const walk = (
+    maze: string[],
+    wall: string,
+    start: Point,
+    end: Point,
+    path: Point[],
+    location = start,
+) => {
+    // BASE CASE(S)
+    //      if we're at the end
+    if (location.x === end.x && location.y === end.y) {
+        return location;
+        //      WE'RE AT A WALL
+    } else if (locationIsOutsideMaze(location, maze)) {
+        //      WE'RE OFF THE BOARD
+        return;
+        //      WE'VE VISITED THIS SPOT BEFORE
+    } else if (alreadyVisited(maze, location)) {
+        return;
+    }
+    // RESURSIVE CASE
+    //      GO NORTH
+    //      GO SOUTH
+    //      GO WEST
+    //      GO EAST
+    //          RECORD CURRENT POSITION
+    return;
+};
+
 export default function solve(
     maze: string[],
     wall: string,
