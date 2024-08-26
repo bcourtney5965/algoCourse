96eeadc3e03268279adc40dc61d4217b4b38d4f5
Brandon Courtney <brandon.h.courtney@gmail.com>
2024-08-26 17:10:26 -0600

    MazeSolver   base cases complete

--- src/day7/MazeSolver.ts
+++ src/day7/MazeSolver.ts
@@ -7,32 +7,29 @@ const locationIsOutsideMaze = (location: Point, maze: string[]): boolean => {
     const { x, y } = location;
     const width: number = maze[0]?.length || 0;
     const height: number = maze.length || 0;
-    return x < 0 || x > width - 1 || y < 0 || y > height - 1;
-};
-
-const alreadyVisited = (maze: string[], location: Point): boolean => {
-    return maze[location.y][location.x] === "1";
+    return x < 0 || x >= width || y < 0 || y >= height;
 };
 
 const walk = (
     maze: string[],
     wall: string,
-    start: Point,
+    curr: Point,
     end: Point,
-    path: Point[],
-    location = start,
-) => {
+    seen: boolean[][],
+): boolean => {
     // BASE CASE(S)
     //      if we're at the end
-    if (location.x === end.x && location.y === end.y) {
-        return location;
-        //      WE'RE AT A WALL
-    } else if (locationIsOutsideMaze(location, maze)) {
+    if (curr.x === end.x && curr.y === end.y) {
+        return true;
         //      WE'RE OFF THE BOARD
-        return;
-        //      WE'VE VISITED THIS SPOT BEFORE
-    } else if (alreadyVisited(maze, location)) {
-        return;
+    } else if (locationIsOutsideMaze(curr, maze)) {
+        return false;
+        //      WE'VE ALREADY VISITED THIS POSITION
+    } else if (seen[curr.y][curr.x]) {
+        return false;
+        //      WE'RE AT A WALL
+    } else if (maze[curr.y][curr.x] === wall) {
+        return false;
     }
     // RESURSIVE CASE
     //      GO NORTH
