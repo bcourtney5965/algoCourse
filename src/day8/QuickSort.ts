da304a9c6039760a8aa9b3c470190a0a8d21b25f
Brandon Courtney <brandon.h.courtney@gmail.com>
2024-08-29 18:05:16 -0600

    QuickSort   - partition helper func

--- src/day8/QuickSort.ts
+++ src/day8/QuickSort.ts
@@ -1,3 +1,20 @@
-export default function quick_sort(arr: number[]): void {
+const qs = (arr: number[], low: number, high: number): void => {};
 
+const partition = (arr: number[], lo: number, hi: number): number => {
+    const pivot = arr[hi];
+
+    let idx = lo - 1;
+    for (let i = lo; i < hi; ++i) {
+        if (arr[i] <= pivot) {
+            idx++;
+            const tmp = arr[i];
+        }
+    }
+    arr[hi] = arr[idx];
+    arr[idx] = pivot;
+    return idx;
+};
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
 }
