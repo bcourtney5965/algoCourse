e2bf7c374a875d8e05e29379fc201fc4330b2ac6
Brandon Courtney <brandon.h.courtney@gmail.com>
2024-08-30 23:58:50 -0600

    QuickSort   - qs helper

--- src/day8/QuickSort.ts
+++ src/day8/QuickSort.ts
@@ -1,17 +1,25 @@
-const qs = (arr: number[], low: number, high: number): void => {};
+const qs = (arr: number[], lo: number, hi: number): void => {
+    if (lo >= hi) {
+        return;
+    }
+    const pivotIdx = partition(arr, lo, hi);
+    qs(arr, lo, pivotIdx - 1);
+    qs(arr, pivotIdx + 1, hi);
+};
 
 const partition = (arr: number[], lo: number, hi: number): number => {
     const pivot = arr[hi];
 
     let idx = lo - 1;
+
     for (let i = lo; i < hi; ++i) {
         if (arr[i] <= pivot) {
             idx++;
             const tmp = arr[i];
+            arr[hi] = arr[idx];
+            arr[idx] = pivot;
         }
     }
-    arr[hi] = arr[idx];
-    arr[idx] = pivot;
     return idx;
 };
 
