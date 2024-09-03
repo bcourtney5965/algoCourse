8ba212fa77dd181a44acdcea90b9f45e4ca6dfb0
Brandon Courtney <brandon.h.courtney@gmail.com>
2024-09-02 21:25:55 -0600

    MazeSolve   - outline walk helper func

--- src/day9/MazeSolver.ts
+++ src/day9/MazeSolver.ts
@@ -3,6 +3,21 @@ type Point = {
     y: number;
 };
 
+const walk = (
+    start: Point,
+    end: Point,
+    path: Point[],
+    wall: string,
+): boolean => {
+    // BASE CASES
+    // if at the end
+    // if outside
+    // if at a wall
+    // if already visited
+    // RECURSIVE CASE
+    return false;
+};
+
 export default function solve(
     maze: string[],
     wall: string,
