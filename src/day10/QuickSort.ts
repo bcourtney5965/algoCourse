592445767d244c030b3c8933af438e411d579ff1
Brandon Courtney <brandon.h.courtney@gmail.com>
2024-11-08 17:01:52 -0700

    Quicksort partition func

--- src/day10/QuickSort.ts
+++ src/day10/QuickSort.ts
@@ -1,6 +1,19 @@
-const qs = (arr: number[], lo: number, high: number): void => {};
+const qs = (arr: number[], lo: number, hi: number): void => {};
 
-const partition = (arr: number[], lo: number, high: number): number => {};
+const partition = (arr: number[], lo: number, hi: number): number => {
+    const pivot = arr[hi];
+    let idx = lo - 1;
+    for (let i = lo; i < hi; ++i) {
+        if (arr[i] <= pivot) {
+            ++idx;
+            [arr[i], arr[idx]] = [arr[idx], arr[i]];
+        }
+    }
+
+    ++idx;
+    arr[hi] = arr[idx];
+    arr[idx] = pivot;
+};
 
 export default function quick_sort(arr: number[]): void {
     qs(arr, 0, arr.length - 1);
