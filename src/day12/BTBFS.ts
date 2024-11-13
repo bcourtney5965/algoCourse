772d1dd0c54ecc3020c5d8d2f8b1d99477ab906a
Brandon Courtney <brandon.h.courtney@gmail.com>
2024-11-13 16:16:57 -0700

    Binary Tree Breadth-First Search

--- src/day12/BTBFS.ts
+++ src/day12/BTBFS.ts
@@ -1,5 +1,31 @@
-function walk(curr: BinaryNode<number> | null, needle: number): boolean {}
+function walk(
+    curr: BinaryNode<number> | null,
+    needle: number,
+    list: BinaryNode<number>[],
+): boolean {
+    if (!curr) return false;
+
+    // if found
+    if (curr.value === needle) return true;
+
+    // add left
+    if (curr.left) {
+        list.push(curr.left);
+    }
+    // add right
+    if (curr.right) {
+        list.push(curr.right);
+    }
+
+    const node = list.shift();
+    if (node) {
+        // recurse
+        return walk(node, needle, list);
+    }
+
+    return false;
+}
 
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
-    return walk(head, needle) || false;
+    return walk(head, needle, []);
 }
