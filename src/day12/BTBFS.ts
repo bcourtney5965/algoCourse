dfc36e6af9ddd43f0c0754f006c0f6ea4b244679
Brandon Courtney <brandon.h.courtney@gmail.com>
2024-11-14 11:13:30 -0700

    Setup iterative breadth-first search

--- src/day12/BTBFS.ts
+++ src/day12/BTBFS.ts
@@ -1,31 +1,55 @@
-function walk(
-    curr: BinaryNode<number> | null,
-    needle: number,
-    list: BinaryNode<number>[],
-): boolean {
-    if (!curr) return false;
+class Node<N> {
+    value: N;
+    next: Node<N> | null;
 
-    // if found
-    if (curr.value === needle) return true;
-
-    // add left
-    if (curr.left) {
-        list.push(curr.left);
+    constructor(value: N) {
+        this.value = value;
+        this.next = null;
     }
-    // add right
-    if (curr.right) {
-        list.push(curr.right);
-    }
-
-    const node = list.shift();
-    if (node) {
-        // recurse
-        return walk(node, needle, list);
-    }
-
-    return false;
 }
 
+class Queue<T> {
+    first: Node<T> | null;
+    last: Node<T> | null;
+    size: number;
+
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    // The enqueue method receives a value and adds it to the "end" of the queue
+    enqueue(val: T) {
+        var newNode = new Node(val);
+        if (!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else if (this.last) {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        return ++this.size;
+    }
+
+    dequeue() {
+        if (!this.first) return null;
+
+        var temp = this.first;
+        if (this.first === this.last) {
+            this.first = this.last = null;
+        }
+        if (this.first) {
+            this.first = this.first.next;
+            this.size--;
+        }
+        return temp.value;
+    }
+}
+
+function breadthFirstSearch(head: BinaryNode<number>, needle: number) {}
+
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
-    return walk(head, needle, []);
+    // const nodes = new Queue<number | null>();
+    // return breadthFirstSearch(head, needle, nodes);
+    return breadthFirstSearch(head, needle);
 }
