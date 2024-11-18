e7b519bf1811ae2d9f7cc11bea995909867e6ee4
Brandon Courtney <brandon.h.courtney@gmail.com>
2024-11-18 14:28:24 -0700

    Implement DFSOnBST

--- src/day14/DFSOnBST.ts
+++ src/day14/DFSOnBST.ts
@@ -1,3 +1,21 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+export default function dfs(
+    head: BinaryNode<number> | null,
+    needle: number,
+): boolean {
+    // if head === null
+    if (!head) {
+        return false;
+    }
 
+    // if found
+    if (head.value === needle) {
+        return true;
+    }
+
+    // if less than needle
+    if (needle < head?.value) {
+        return dfs(head.left, needle);
+    }
+
+    return dfs(head.right, needle);
 }
