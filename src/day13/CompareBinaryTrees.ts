4818eeeb6183ea6aaf68349a1482367b02624144
Brandon Courtney <brandon.h.courtney@gmail.com>
2024-11-18 14:15:48 -0700

    Implement CompareBinaryTrees

--- src/day13/CompareBinaryTrees.ts
+++ src/day13/CompareBinaryTrees.ts
@@ -1,13 +1,19 @@
-function worker(
-    a: BinaryNode<number> | null,
-    b: BinaryNode<number> | null,
-): boolean {
-    return true;
-}
-
 export default function compare(
     a: BinaryNode<number> | null,
     b: BinaryNode<number> | null,
 ): boolean {
-    return worker(a, b);
+    // if both null
+    if (a === null && b === null) return true;
+    // return true
+
+    if (a === null || b === null) {
+        return false;
+    }
+
+    // if number's match return true, else return false
+    if (a?.value !== b?.value) {
+        return false;
+    }
+
+    return compare(a?.left, b?.left) && compare(a?.right, b?.right);
 }
