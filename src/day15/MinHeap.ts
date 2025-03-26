f66e430cbf6ee2e15d619c68fde868735dcdc7cb
Brandon Courtney <brandon.h.courtney@gmail.com>
2025-03-26 16:30:53 -0600

    MinHeap insert() method naive solution

--- src/day15/MinHeap.ts
+++ src/day15/MinHeap.ts
@@ -1,8 +1,43 @@
 export default class MinHeap {
   public length: number;
+  public heap: Array<number>;
 
-  constructor() {}
+  constructor() {
+    this.heap = [];
+    this.length = 0;
+  }
 
-  insert(value: number): void {}
+  private getParentIndex(index: number): number {
+    return Math.floor((index - 1) / 2);
+  }
+
+  private incrementLength(): void {
+    this.length++;
+  }
+
+  insert(value: number): void {
+    // push it onto the back
+    this.heap.push(value);
+    this.incrementLength();
+
+    // initiate variables
+    let valIndex = this.heap.length - 1;
+    let parentIndex = this.getParentIndex(valIndex);
+
+    // while it's smaller than its parent
+    while (value < this.heap[parentIndex]) {
+      //      swap with parent
+      [this.heap[valIndex], this.heap[parentIndex]] = [
+        this.heap[parentIndex],
+        this.heap[valIndex],
+      ];
+
+      // update valIndex ref's
+      valIndex = parentIndex;
+      parentIndex = this.getParentIndex(valIndex);
+      if (parentIndex < 0 || valIndex < 0) break;
+    }
+    return;
+  }
   delete(): number {}
 }
